f20d2cc04960de59e7a0f778fd528278155303d7
miggy-pg <meelan1323@gmail.com>
2023-07-30 07:10:02 +0800

    [UPDATE] push cooking_steps

---
+++ src/data/food_recipes.js
@@ -0,0 +1,69 @@
+import cherry_poke_cake from "./1-2-3-cherry-poke-cake.json";
+import jambalaya from "./1-2-3-jambalaya.json";
+import lemon_icebox_pie from "./1-2-3-lemon-icebox-pie.json";
+import chicken_parmesan from "./1-dish-chicken-parmesan.json";
+import pepperoni_pizza from "./1-dish-pepperoni-cheese-pizza-bake.json";
+import dish_taco from "./1-dish-taco-bake.json";
+import pea_salad_most from "./1-pea-salad-most-requested.json";
+import pumpkin_spice_cookies from "./1-pumpkin-spice-cookies.json";
+import chipotle_hummus from "./1-step-chipotle-hummus.json";
+import chocolate_mug_cake from "./10-minute-chocolate-mug-cake.json";
+import rice_cake from "./10-minute-rice-cakes.json";
+import tomato_basil_salad from "./10-minute-tomato-basil-salad.json";
+import cheesecake from "./10-pound-cheesecake.json";
+import can_chili from "./11-can-chili.json";
+import coleslaw from "./12-second-coleslaw.json";
+import manhattan from "./12th-man-hattan.json";
+import nachos_supreme from "./15-minute-dinner-nachos-supreme.json";
+import herbed_chicken from "./15-minute-herbed-chicken.json";
+import pickled_beets from "./15-minute-pickled-beets-kitchen-hack.json";
+import pesto_sauce from "./15-year-old-pesto-sauce.json";
+import wheat_bread from "./100-percent-whole-wheat-bread.json";
+import wheat_pancakes from "./100-whole-wheat-pancakes.json";
+import wheat_peanut_butter from "./100-whole-wheat-peanut-butter-and-je.json";
+import whaler_fish_sandwhich from "./101-whaler-fish-sandwich-239757.json";
+import calorie_peach_pie from "./120-calorie-peach-pies.json";
+import whipped_cream from "./121-whipped-cream.json";
+import green_time_ice_cream from "./123-green-tea-ice-cream.json";
+import mocha from "./123-mocha-2-go.json";
+import french_strawberry_pie from "./1970s-french-strawberry-pie.json";
+import chicken_pie from "./1985-chicken-pie-with-biscuit-crust-11056.json";
+import chocolate_raspberry_almond from "./1989-chocolate-raspberry-almond-torte-11037.json";
+import maryland_crab_cake from "./19489.json";
+
+let food_recipes = [
+  cherry_poke_cake,
+  jambalaya,
+  lemon_icebox_pie,
+  chicken_parmesan,
+  pepperoni_pizza,
+  dish_taco,
+  pea_salad_most,
+  pumpkin_spice_cookies,
+  chipotle_hummus,
+  chocolate_mug_cake,
+  rice_cake,
+  tomato_basil_salad,
+  cheesecake,
+  can_chili,
+  coleslaw,
+  manhattan,
+  nachos_supreme,
+  herbed_chicken,
+  pickled_beets,
+  pesto_sauce,
+  wheat_bread,
+  wheat_pancakes,
+  wheat_peanut_butter,
+  whaler_fish_sandwhich,
+  calorie_peach_pie,
+  whipped_cream,
+  green_time_ice_cream,
+  mocha,
+  french_strawberry_pie,
+  chicken_pie,
+  chocolate_raspberry_almond,
+  maryland_crab_cake,
+];
+
+export default food_recipes;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,93 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.scss";
+import food_recipes from "./data/food_recipes";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function Steps() {
+  return (
+    <h3 className="card-hover__title">
+      Make your <span>choice</span> right now!
+    </h3>
+  );
+}
+
+function Food() {
+  return (
+    <>
+      <Steps />
+      <p className="card-hover__text">
+        Lorem ipsum, dolor sit amet cssonsectetur adipisicing elit. Officia
+        quisquam doloremque nostrum laboriosam, blanditiis libero corporis nulla
+        a aut?
+      </p>
+    </>
+  );
+}
+
+function Buttons() {
+  return (
+    <>
+      <div className="card-hover__buttons">
+        <button href="#" className="previous round">
+          Previous
+        </button>
+        <button className="next round">Next</button>
+      </div>
+    </>
+  );
+}
+
+function Menu({ name }) {
+  return (
+    <>
+      <li>
+        <img src="https://www.iconpacks.net/icons/2/free-arrow-right-icon-2817-thumb.png" />
+        <span>{name}</span>
+        <button onClick={""}>Show Recipe</button>
+      </li>
+    </>
+  );
+}
+
+function CookBook() {
+  return (
+    <div className="card-hover">
+      {/* TITLE */}
+      <div className="card-hover__cookbook_title_wrapper">
+        <img src="https://www.svgrepo.com/show/124022/cookbook.svg" />
+        <span className="card-hover__cookbook_title">Livre De Cuisine</span>
+        <p>
+          "Cooking is not only an essential skill for nourishing our bodies but
+          also a creative and therapeutic activity that deepens our connection
+          with the food we eat."
+        </p>
+      </div>
+
+      {/* CONTENT */}
+      <div className="card-hover__content">
+        {food_recipes.length > 0 ? (
+          <ol>
+            {food_recipes.map((food) => (
+              <Menu name={food.title} key={food.title} />
+            ))}
+          </ol>
+        ) : (
+          <span>
+            Sorry to inform you but we don't have any recipes prepared for you.
+          </span>
+        )}
+
+        {/* <Food />
+        <Buttons /> */}
+      </div>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <CookBook />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
